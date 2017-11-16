0d811f100230af3565f75a4b7c8d18d9e99cc25b
Tin Chu <mchu.tinho@gmail.com>
2017-11-16 16:53:13 -0600

    refacto logic

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -7,9 +7,9 @@ import * as actions from '../actions';
 
 class ListItem extends Component {
 	renderDescription() {
-		const { library, selectedLibraryId } = this.props;
+		const { expanded, library} = this.props;
 
-		if (library.id === selectedLibraryId) {
+		if (expanded) {
 			return (
 				<Text>{library.description}</Text>
 			);
@@ -43,10 +43,9 @@ const styles = {
 	}
 };
 
-const mapStateToProps = state => {
-	return {
-		selectedLibraryId: state.selectedLibraryId
-	};
+const mapStateToProps = (state, ownProps) => {
+	const expanded = state.selectedLibraryId === ownProps.library.id;
+	return { expanded };
 };
 
 export default connect(mapStateToProps, actions)(ListItem);
